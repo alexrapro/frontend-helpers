9315d31ce37a90ef25c3755c7555ada74221032f
alexrapro <alex-rapro@yandex.ru>
2023-05-10 15:06:53 +0700

    transformHits

---
+++ helpers/transformHits.ts
@@ -0,0 +1,17 @@
+import {SearchHit} from "@elastic/elasticsearch/lib/api/types";
+
+const transformHits = (data?: SearchHit[] | string[]) => {
+    if (data?.length) {
+        return data?.map((entity) => {
+            if (typeof entity === "string") {
+                return entity
+            } else {
+                return entity._id
+            }
+        })
+    } else {
+        return []
+    }
+}
+
+export default transformHits
