4f119bb6f9e033ee66c4703a0e56c9506b77d611
alexrapro <alex-rapro@yandex.ru>
2023-09-13 10:23:43 +0700

    add objectCleaner

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import removeEmptyProps from "./helpers/removeEmptyProps";
 import transformHits from "./helpers/transformHits";
 import removeUnderscoreProps from "./helpers/removeUnderscoreProps";
+import objectCleaner from "./helpers/objectCleaner";
 import useQueryString from "./hooks/useQueryString";
 
 export {
     removeEmptyProps,
     removeUnderscoreProps,
     transformHits,
-    useQueryString
+    useQueryString,
+    objectCleaner
 }
