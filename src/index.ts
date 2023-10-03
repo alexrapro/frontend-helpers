374ea7533ad23a7a29cca7a34accb5aad0b0ee6a
alexrapro <alex-rapro@yandex.ru>
2023-10-03 08:36:13 +0700

    fix import

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import transformHits from "./helpers/transformHits";
 import removeUnderscoreProps from "./helpers/removeUnderscoreProps";
 import objectCleaner from "./helpers/objectCleaner";
 import useQueryString from "./hooks/useQueryString";
+import useSearchRouterNext from "./hooks/useSearchRouterNext";
 
 export {
     removeEmptyProps,
     removeUnderscoreProps,
     transformHits,
     useQueryString,
+    useSearchRouterNext,
     objectCleaner
 }
