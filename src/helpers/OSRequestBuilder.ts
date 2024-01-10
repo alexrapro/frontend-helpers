ca80a904528a0ad1b7284893d852e69a167fdf02
alexrapro <alex-rapro@yandex.ru>
2024-01-10 16:24:04 +0700

    filters as array

--- src/helpers/OSRequestBuilder.ts
+++ src/helpers/OSRequestBuilder.ts
@@ -1,13 +1,20 @@
 import {QueryDslQueryContainer, SearchRequest} from "@opensearch-project/opensearch/api/types";
 import {removeEmptyProps} from "../index";
 
-export const filterBuilder = (request: SearchRequest['body'], filter: QueryDslQueryContainer): SearchRequest['body'] => (removeEmptyProps({
-    ...request,
-    query: {
-        ...request?.query,
-        bool: {
-            ...request?.query?.bool,
-            filter: request?.query?.bool?.filter ? [...request.query.bool.filter as QueryDslQueryContainer[], filter] : [filter]
+export const filterBuilder = (request: SearchRequest['body'], filters: QueryDslQueryContainer | QueryDslQueryContainer[]): SearchRequest['body'] => {
+    let filter: QueryDslQueryContainer[] = request?.query?.bool?.filter ? [...request.query.bool.filter as QueryDslQueryContainer[]] : []
+
+    if (Array.isArray(filters)) filters.map(item => filter.push(item))
+    else filter.push(filters)
+
+    return removeEmptyProps({
+        ...request,
+        query: {
+            ...request?.query,
+            bool: {
+                ...request?.query?.bool,
+                filter
+            }
         }
-    }
-}))
+    })
+}
